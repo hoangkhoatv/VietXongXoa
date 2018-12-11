902de3420bd2fae1dae7e0e4f68c6f8233e8ec75
Dinh Nhat Bang <dinhnhatbang@gmail.com>
2018-12-11 10:15:29 +0700

    Optimize code

--- app/src/main/java/com/vietxongxoa/injection/module/ApplicationModule.java
+++ app/src/main/java/com/vietxongxoa/injection/module/ApplicationModule.java
@@ -1,19 +1,3 @@
-/*
- *    Copyright (C) 2018 MINDORKS NEXTGEN PRIVATE LIMITED
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
-
 package com.vietxongxoa.injection.module;
 
 import android.app.Application;
@@ -24,13 +8,10 @@ import com.vietxongxoa.injection.annotation.ApplicationContext;
 import dagger.Module;
 import dagger.Provides;
 
-/**
- * Created by amitshekhar on 13/01/17.
- */
 @Module
 public class ApplicationModule {
 
-    protected final Application mApplication;
+    private final Application mApplication;
 
     public ApplicationModule(Application application) {
         mApplication = application;
@@ -46,5 +27,4 @@ public class ApplicationModule {
     Context provideContext() {
         return mApplication;
     }
-
 }
