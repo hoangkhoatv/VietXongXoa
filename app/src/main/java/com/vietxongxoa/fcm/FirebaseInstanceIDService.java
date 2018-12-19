d83ef8f54a77dd370db1a89f29aa7cc1abc55b2d
Dinh Nhat Bang <dinhnhatbang@gmail.com>
2018-12-19 22:17:42 +0700

    Clean code FCM

--- app/src/main/java/com/vietxongxoa/fcm/FirebaseInstanceIDService.java
+++ app/src/main/java/com/vietxongxoa/fcm/FirebaseInstanceIDService.java
@@ -14,13 +14,9 @@ public class FirebaseInstanceIDService extends FirebaseInstanceIdService {
     @SuppressLint("LongLogTag")
     @Override
     public void onTokenRefresh() {
-        // Get updated InstanceID token.
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
         Log.d(TAG, "Refreshed token: " + refreshedToken);
 
-        // If you want to send messages to this application instance or
-        // manage this apps subscriptions on the server side, send the
-        // Instance ID token to your app server.
         sendRegistrationToServer(refreshedToken);
     }
 
